955e4550711d2373fa7de0863166e03638377c1f
Aaron-Gregoire <aaronjemmett@gmail.com>
2026-02-15 17:07:25 -0500

    done practice 1

--- src/wk3/Movie.java
+++ src/wk3/Movie.java
@@ -1,4 +1,29 @@
 package wk3;
 
 public class Movie {
+
+    private String movieName;
+    private int[] ratings;
+
+    public Movie(String name, int[] movieRatings){
+        this.movieName = name;
+        this.ratings = movieRatings;
+    }
+   public double calculateAverageRating(){
+        int sum = 0;
+       for (int i = 0; i < ratings.length; i++) {
+           sum = sum + ratings[i];
+
+       }
+       return sum / ratings.length;
+   }
+   public void displayMovieDetails(){
+       System.out.println("Movie: " + movieName);
+       System.out.println("Ratings: ");
+       for (int i = 0; i < ratings.length; i++){
+           System.out.println(ratings[i] + " ");
+       }
+       System.out.println("Average Rating: " + calculateAverageRating());
+   }
+
 }
--- src/wk3/Tester.java
+++ src/wk3/Tester.java
@@ -2,7 +2,11 @@ package wk3;
 
 public class Tester {
 
+    public static void main(String[] args){
+        int[] ratings = {3, 4, 5, 3, 2};
 
-    Tester Movie = new Tester();
-
+        Movie movie1 = new Movie("inception", ratings);
+         movie1.displayMovieDetails();
+        System.out.println("Calculated average: " + movie1.calculateAverageRating());
+    }
 }
